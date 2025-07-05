1058fcaea9f6298f300fa6f8bb25af18c8e52643
Amal Saju Kalarickal <amalsaju1998@gmail.com>
2025-07-05 15:31:55 +1000

    feat: Added ability to increase/decrease rows and columns

--- game-of-life/src/app/_components/cell.tsx
+++ game-of-life/src/app/_components/cell.tsx
@@ -1,12 +1,12 @@
 'use client';
-import { useState } from "react";
+import { MouseEventHandler, useState } from "react";
 import { CellState } from "../utils";
 
 // 1 == Alive, 0 == Dead 
-const Cell = ({ currentState }: { currentState: number }) => {
+const Cell = ({ currentState, clickHandler, mouseDownHandler }: { currentState: number, clickHandler: MouseEventHandler, mouseDownHandler: MouseEventHandler }) => {
 
   return (
-    <div className={`w-5 h-5 border-1 border-black ${currentState == 1 ? "bg-black" : "bg-white"}`}>
+    <div onClick={clickHandler} onMouseEnter={mouseDownHandler} className={`w-5 h-5 border-1 border-black ${currentState == 1 ? "bg-black" : "bg-white"}`}>
     </div>
   )
 }
