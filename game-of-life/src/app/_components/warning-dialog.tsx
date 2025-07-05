cbd0a45fc68c57908e8285fbfed367ef4ec606b0
Amal Saju Kalarickal <amalsaju1998@gmail.com>
2025-07-05 17:29:12 +1000

    Feat: Added some extra sytling

--- game-of-life/src/app/_components/warning-dialog.tsx
+++ game-of-life/src/app/_components/warning-dialog.tsx
@@ -20,12 +20,12 @@ const WarningDialog = () => {
   return (
     <>
       {isMobileDevice && (
-        <dialog open={true} className="w-screen h-screen bg-none flex items-center justify-center backdrop-blur-xs bg-gray-600/90">
-          <div className="bg-indigo-500 border-2 border-indigo-950 rounded-md w-9/10 p-2 flex flex-col items-center">
+        <dialog open={true} className="w-screen h-screen bg-none flex items-center justify-center backdrop-blur-xs bg-gray-600/90 ">
+          <div className="bg-indigo-500 border-1 border-indigo-950 rounded-md w-9/10 p-2 flex flex-col items-center shadow-lg/30">
             <p className="p-2 text-white text-xl">
               PC or laptop is recommended if you are using a smartphone.
             </p>
-            <button className=" w-fit border-1 border-bg-indigo-200 rounded-md text-xl text-white p-2" onClick={buttonClick}>Close</button>
+            <button className=" w-fit border-2 border-indigo-950 rounded-md text-xl text-white p-2" onClick={buttonClick}>Close</button>
           </div>
         </dialog>
       )}
